e7689d8dd457513461a8bd7b50d6f234f086c752
heyx <730530507@qq.com>
2017-12-05 21:21:15 +0800

    1

--- Config/MyConfig.cs
+++ Config/MyConfig.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace webapi.Config
-{
+{ 
     public class MyConfig
     {
         public string Name { set; get; }
--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM microsoft/dotnet:latest
 WORKDIR /publish
 EXPOSE 9521
 COPY . /publish 
-ENTRYPOINT ["dotnet", "webapi.dll"]
+ENTRYPOINT ["dotnet", "webapi.dll"] 
