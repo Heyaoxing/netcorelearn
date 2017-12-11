f83668728f1cda9f2ad7d94d17d20c4a3ddaa3ec
heyx <730530507@qq.com>
2017-12-11 17:58:16 +0800

    1

---
+++ A_Core/Core.Log/Expressions/Warning.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Uitl.Expressions
+{
+    public class Warning : Exception
+    {
+        private readonly string _message;
+
+        /// <summary>
+        /// 错误码
+        /// </summary>
+        public string Code { get; set; }
+
+
+        /// <summary>
+        /// 初始化应用程序异常
+        /// </summary>
+        /// <param name="message">错误消息</param>
+        public Warning(string message) : this(message, "")
+        {
+
+        }
+
+        /// <summary>
+        /// 初始化应用程序异常
+        /// </summary>
+        /// <param name="message">错误消息</param>
+        /// <param name="code">错误码</param>
+        public Warning(string message, string code) : this(message, code, null)
+        {
+
+        }
+
+        /// <summary>
+        /// 初始化应用程序异常
+        /// </summary>
+        /// <param name="message">错误消息</param>
+        /// <param name="code">错误码</param>
+        /// <param name="exception">异常</param>
+        public Warning(string message, string code, Exception exception) : base(message, exception)
+        {
+            Code = code;
+            _message = GetMessage();
+        }
+
+        /// <summary>
+        /// 初始化应用程序异常
+        /// </summary>
+        /// <param name="message">错误消息</param>
+        /// <param name="code">错误码</param>
+        /// <param name="exception">异常</param>
+        private string GetMessage()
+        {
+            var result = new StringBuilder();
+            AppendSelfMessage(result);
+            AppendInnerMessage(result, InnerException);
+            return result.ToString().TrimEnd(Environment.NewLine.ToCharArray());
+        }
+
+        /// <summary>
+        /// 添加外层异常消息
+        /// </summary>
+        private void AppendSelfMessage(StringBuilder result)
+        {
+            if (string.IsNullOrWhiteSpace(base.Message))
+                return;
+            result.AppendLine(base.Message);
+        }
+
+        /// <summary>
+        /// 添加内部异常消息
+        /// </summary>
+        private void AppendInnerMessage(StringBuilder result, Exception exception)
+        {
+            if (exception == null)
+                return;
+
+            if (exception is Warning)
+            {
+                result.AppendLine(exception.Message);
+                return;
+            }
+
+            result.AppendLine(exception.Message);
+            AppendData(result, exception);
+            AppendInnerMessage(result, exception.InnerException);
+        }
+
+        /// <summary>
+        /// 添加额外数据
+        /// </summary>
+        private void AppendData(StringBuilder result, Exception exception)
+        {
+            foreach (DictionaryEntry date in exception.Data)
+            {
+                result.AppendFormat("{0}:{1}{2}", date.Key, date.Value, Environment.NewLine);
+            }
+        }
+
+        /// <summary>
+        /// 错误消息
+        /// </summary>
+        public override string Message
+        {
+            get
+            {
+                if (Data.Count == 0)
+                    return _message;
+                StringBuilder result = new StringBuilder();
+                result.AppendLine(_message);
+                AppendData(result, this);
+                return result.ToString();
+            }
+        }
+
+        /// <summary>
+        /// 堆栈跟踪
+        /// </summary>
+        public override string StackTrace
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(base.StackTrace))
+                    return base.StackTrace;
+
+                if (base.InnerException == null)
+                    return string.Empty;
+
+                return base.InnerException.StackTrace;
+            }
+        }
+
+        /// <summary>
+        /// 获取友情提示
+        /// </summary>
+        /// <param name="level">日志级别</param>
+        public string GetPrompt(LogLevel level)
+        {
+            if (level == LogLevel.Error)
+            {
+                return R.SystemError;
+            }
+            return Message;
+        }
+    }
+}
---
+++ A_Core/Core.Log/Log/Implement/LogContext.cs
@@ -0,0 +1,48 @@
+﻿using Core.Uitl.Log.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using Core.Uitl.Contexts;
+
+namespace Core.Uitl.Log.Implement
+{
+    public class LogContext : ILogContext
+    {
+        /// <summary>
+        /// 日志上下文信息
+        /// </summary>
+        private LogContextInfo _info;
+
+        /// <summary>
+        /// 上下文
+        /// </summary>
+        public IContext Context { get; set; }
+
+        /// <summary>
+        /// 初始化日志上下文
+        /// </summary>
+        /// <param name="context">上下文</param>
+        public LogContext(IContext context)
+        {
+            Context = context;
+        }
+
+        //private LogContextInfo GetInfo()
+        //{
+
+        //}
+
+        public string TraceId => throw new NotImplementedException();
+
+        public Stopwatch Stopwatch => throw new NotImplementedException();
+
+        public string Ip => throw new NotImplementedException();
+
+        public string Host => throw new NotImplementedException();
+
+        public string Browser => throw new NotImplementedException();
+
+        public string Url => throw new NotImplementedException();
+    }
+}
